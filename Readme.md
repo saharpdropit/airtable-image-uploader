ad8b86929fb091cc655064101fea35e2129bede1
saharperetz <saharp@dropitshopping.com>
2022-05-30 17:05:18 +0300

    airtable.js is shit, so used axios

--- Readme.md
+++ Readme.md
@@ -1,13 +1,14 @@
 ### Required env vars:
+
 ```
 NGROK_URL - current ngrok url (See https://ngrok.com/)
 
 AIRTABLE_API_KEY - airtable api key (See https://airtable.com/account)
 AIRTABLE_BASE - airtable base id
-
 ```
 
 ### Execution:
+
 - Configure your env vars
 - Create 'images' folder inside 'public' folder
 - Add the images you want to upload to airtable to /public/images/
--- image-uploader.js
+++ image-uploader.js
@@ -1,4 +1,4 @@
-const airtable = require("airtable");
+const axios = require("axios").default;
 
 const fs = require("fs");
 const path = require("path");
@@ -7,86 +7,109 @@ const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
 const NGROK_URL = process.env.NGROK_URL;
 
-airtable.configure({
-  endpointUrl: "https://api.airtable.com",
-  apiKey: AIRTABLE_API_KEY,
-});
-
-const base = airtable.base(AIRTABLE_BASE_ID);
+const baseUrl = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}`
 
 const sourceDir = path.join(__dirname, "public", "images");
 const images = [...fs.readdirSync(sourceDir)];
 
 let recordKeys = new Set();
 
-const getFeaturesRecords = async () => {
-  return await base.table("Features").select({ view: "Grid view" }).all();
+const uploadImages = async(records) => {
+    const featuresRecords = await fetchRecords("Features");
+
+    for (const record of records) {
+        const productId = record.fields["Product ID"];
+        const colorCode = String(record.fields["Color Code"]).padStart(4, 0);
+
+        const processedImages = record.fields["Processed Images"];
+
+        const currentRecordsDescription = getRelatedDescription(featuresRecords, productId);
+        if (currentRecordsDescription) {
+
+            const description = {
+                fields: {
+                    ["Description (HTML) *"]: currentRecordsDescription
+                }
+            }
+
+            await updateRecord("Products", record.id, description)
+        }
+
+        const productIdAndColorCode = `${productId} ${colorCode}`;
+
+        if (recordKeys.has(productIdAndColorCode)) continue;
+
+        recordKeys.add(productIdAndColorCode);
+
+        if (processedImages && processedImages.length > 0) {
+            console.log("skipping record, already has processed images...");
+            continue;
+        }
+
+        // remove / edit the.includes("Detail") by demand
+        const relatedRecordImages = images.filter((image) =>
+            image.includes(productIdAndColorCode) && !image.includes("Detail")
+        );
+
+        const imagesUrl = [];
+
+        for (const relatedRecordImage of relatedRecordImages) {
+            const urlPath = encodeURI(`${NGROK_URL}/images/${relatedRecordImage}`);
+            imagesUrl.push(urlPath);
+        }
+
+        const relatedImages = {
+            fields: {
+                ["Processed Images"]: imagesUrl.map(imageUrl => { return { url: imageUrl } })
+            }
+        }
+
+        await updateRecord("Products", record.id, relatedImages)
+    }
 };
 
-const uploadImages = async (records) => {
-  const featuresRecords = await getFeaturesRecords();
+const updateRecord = async(tableName, recordId, fields) => {
+    const encodedUrl = encodeURI(`${baseUrl}/${tableName}/${recordId}`);
+    return await axios.patch(encodedUrl, fields, { headers: { Authorization: `Bearer ${AIRTABLE_API_KEY}` } });
+};
 
-  for (const record of records) {
-    const processedImages = record.get("Processed Images");
-    const productId = record.get("Product ID");
-    const colorCode = String(record.get("Color Code")).padStart(4, 0);
+const fetchRecords = async(tableName) => {
+    const encodedBaseUrl = encodeURI(`${baseUrl}/${tableName}`);
 
-    const relatedFeaturesRecords = featuresRecords.filter(
-      (featuresRecord) =>
-        (featuresRecord.get("Style Number") == productId &&
-          featuresRecord.get("Feature Name") == "Description") ||
-        featuresRecord.get("Feature Name" == "Features")
+    const records = [];
+
+    let offset = "";
+
+    while (offset != null) {
+        const res = await getRecords(`${encodedBaseUrl}${offset}`);
+
+        if (res.status == 200) {
+            records.push(...res.data.records);
+            offset = tryGetOffset(res.data)
+        }
+    }
+
+    return records;
+}
+
+const tryGetOffset = (res) => res.offset ? `?offset=${res.offset}` : null;
+
+const getRecords = async(url) => await axios.get(url, { headers: { Authorization: `Bearer ${AIRTABLE_API_KEY}` } });
+
+const run = async() => {
+    const records = await fetchRecords("Products", "All Products");
+    return await uploadImages(records);
+};
+
+const getRelatedDescription = (featuresRecords, productId) => {
+    const relatedRecords = featuresRecords.filter((featuresRecord) =>
+        (featuresRecord.fields["Style Number"] == productId) &&
+        (featuresRecord.fields["Feature Name"] == "Description" || featuresRecord.fields["Feature Name"] == "Features")
     );
-    
-    if (relatedFeaturesRecords.length > 0) {
-      const description = relatedFeaturesRecords[0].get("Feature Value");
-      if(description) {
-        base("Products").update(record.id, { 
-          "Description (HTML) *": description
-        });    
-      }
-    }
 
-    const productIdAndColorCode = `${productId} ${colorCode}`;
+    if (relatedRecords.length > 0)
+        return relatedRecords[0].fields["Feature Value"];
 
-    if (recordKeys.has(productIdAndColorCode)) continue;
-
-    recordKeys.add(productIdAndColorCode);
-
-    if (processedImages && processedImages.length > 0) {
-      console.log("skipping record, already has processed images...");
-      continue;
-    }
-
-    // remove/edit the .includes("Detail") by demand
-    const relatedRecordImages = images.filter(
-      (image) =>
-        image.includes(productIdAndColorCode) && !image.includes("Detail")
-    );
-
-    const imagesUrl = [];
-
-    for (const relatedRecordImage of relatedRecordImages) {
-      const urlPath = encodeURI(`${NGROK_URL}/images/${relatedRecordImage}`);
-      imagesUrl.push(urlPath);
-    }
-
-    base("Products").update(record.id, {
-      "Processed Images": imagesUrl.map((imageUrl) => {
-        return { url: imageUrl };
-      }),
-    });
-  }
-};
-
-const run = () => {
-  return base
-    .table("Products")
-    .select({ view: "All Products" })
-    .eachPage(async function page(records, fetchNextPage) {
-      await uploadImages(records);
-      fetchNextPage();
-    });
-};
+}
 
 module.exports = { run };
